128a58271c2631df20fe5b51684f9ef927698012
Alex <cigan@ua.fm>
2020-10-08 13:29:08 +0300

    lesson 9, tabs, add a function to filter elements on a page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import checkTextInputs from './modules/checkTextInputs';
 import culc from './modules/culc';
+import filter from './modules/filter';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import modals from './modules/modals';
@@ -19,5 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // showMoreStyles('.button-styles', '.styles-2');  // заком., используем БД
     showMoreStyles('.button-styles', '#styles .row');
     culc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
 
 });
