010394ddd12f46335aa2fcb50cbdab2d8e77665d
Alex <cigan@ua.fm>
2020-10-08 17:23:55 +0300

    lesson 13,  up and smooth scroll, add a function to go up after clicking on an arrow and does it smoothly

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import modals from './modules/modals';
 import pictureSize from './modules/pictureSize';
+import scrolling from './modules/scrolling';
 import showMoreStyles from './modules/showMoreStyles';
 import sliders from './modules/sliders';
 
@@ -28,5 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // accordion('.accordion-heading ', '.accordion-block');  // css-animation
     accordion('.accordion-heading');  // using js
     burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 
 });
